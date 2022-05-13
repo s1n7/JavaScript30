6f316a31ddca8cc1c365fd684103ad70ae4c1b56
s1n7 <luisbruemmer@gmail.com>
2022-05-13 22:54:14 +0200

    Make styles disappear after while and make sounds play

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,7 +1,18 @@
-const buttons = document.querySelectorAll('.key');
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-buttons.forEach((button) => {
-    button.addEventListener('click', function(e) {
-        button.classList.add('playing');
-    });
-});
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
